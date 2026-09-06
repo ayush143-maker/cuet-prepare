cb92da5ab6e7ae2698a1b4ecfa89c5cc5f5f2f2f
Ayush <rahulbhartimzp7@gmail.com>
2026-09-06 13:54:50 +0530

    Update index.ts

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,6 +1,7 @@
 export * from "./cta-section";
 export * from "./feature-grid";
 export * from "./home-hero";
+export * from "./how-it-works";
 export * from "./mode-grid";
 export * from "./stats-strip";
 export * from "./subject-grid";
