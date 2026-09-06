c13f61f3eb1e67920ff44dcd22871621d29fa80f
Ayush <rahulbhartimzp7@gmail.com>
2026-09-06 10:51:25 +0530

    Export components from home index file

---
+++ src/components/home/index.ts
@@ -0,0 +1,6 @@
+export * from "./cta-section";
+export * from "./feature-grid";
+export * from "./home-hero";
+export * from "./mode-grid";
+export * from "./stats-strip";
+export * from "./subject-grid";
