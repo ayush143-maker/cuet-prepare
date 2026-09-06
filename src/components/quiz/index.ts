f3f3c8bbe34fd61564c31b1c1516b0ed942d30a7
Ayush <rahulbhartimzp7@gmail.com>
2026-09-06 09:29:01 +0530

    Create index.ts to export quiz components

---
+++ src/components/quiz/index.ts
@@ -0,0 +1,7 @@
+export * from "./option-card";
+export * from "./question-card";
+export * from "./question-palette";
+export * from "./quiz-builder";
+export * from "./quiz-header";
+export * from "./submit-modal";
+export * from "./timer-ring";
